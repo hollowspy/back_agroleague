0e461cd789bd9b2335417fa32ebc7bd48bbe6dc8
hollowspy <julien.niedzwiecki@neuf.fr>
2022-04-15 07:25:28 +0200

    Authorize request only from localhost:3000

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({origin: true, credentials: true}))
+app.use(cors({origin: 'http://localhost:3000', credentials: true}))
 
 
 // Use router middleware
